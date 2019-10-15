5e0ccc1c93b99baace3048a94788caf299cfcecc
AlgiGaming <56507922+AlgiGaming@users.noreply.github.com>
2019-10-15 16:08:30 +0700

    Add files via upload

---
+++ algiACJH.sh
@@ -0,0 +1,20 @@
+echo "SELAMAT DATANG DI ACJH SCRIPT"
+echo "Name : AlgiACJH"
+echo "Contact : 085295566302"
+read -p "siapa nama mu? :  " nama
+echo "Selamat Datang $nama di ACJH Script"
+read -p "$nama , apa Script yang kamu butuhkan? :  " sc
+echo "ohh Script $sc"
+sleep 3
+
+
+echo "maaf, Script $sc belum tersedia di script ini"
+echo "jika bertanya script script, langsung hubungi admin di 085295566302"
+sleep 3
+
+
+
+
+echo "TERIMA KASIH :)"
+
+
